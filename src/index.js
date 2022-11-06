833b683f54434d9bb31b20b1889f3263fc7b75e9
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-11-06 19:12:03 +0200

    Add all without stop search

--- src/index.js
+++ src/index.js
@@ -1,56 +1,88 @@
+import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import $ from "jquery";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './fetchCountries.js'
-import debounce from 'lodash.debounce';
+import PixabayAPI from './pixabay-servise';
+import createMarkup from './create-markup-image';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import './css/styles.css';
+AOS.init();
 
-const DEBOUNCE_DELAY = 300;
-const inputEl = document.querySelector('input#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfoBox = document.querySelector('.country-info');
+const searchForm = document.querySelector('form#search-form');
+const imagesContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const mybutton = document.getElementById("myBtn");
 
-// inputEl.addEventListener('input', onCountryInput);
+imagesContainer.addEventListener('click', onAddItemsClick);
+searchForm.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
-inputEl.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
+const pixabayAPI = new PixabayAPI();
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 200, });
 
-function onCountryInput(e) {
-    e.preventDefault()
-    const value = e.target.value.trim();
-    if (value === "") {
-                countryList.innerHTML = '';
-                countryInfoBox.innerHTML = '';
-                return;
+async function onSearchFormSubmit(e) {
+    e.preventDefault();
+    window.addEventListener('scroll', onScrollWindow, {
+    passive: true
+});
+    imagesContainer.innerHTML = "";
+    pixabayAPI.resetPage()
+    pixabayAPI.query = e.currentTarget.elements.searchQuery.value;
+    await featchImages();
+}
+
+async function onLoadMore() {
+    await featchImages();
+}
+
+
+function onScrollWindow() {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        featchImages()
+    }
+} 
+
+
+async function featchImages() {
+    await pixabayAPI.featchImages()
+        .then(images => {
+            loadMoreBtn.classList.remove('is-hidden');
+            if (images.total === 0) {
+                imagesContainer.innerHTML = "";
+                return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            } else {
+                pixabayAPI.incrementPage();
+                const createdImages = images.hits.map(image => createMarkup(image)).join('');
+                imagesContainer.insertAdjacentHTML('beforeend', createdImages);
+                lightbox.refresh();
             }
-    fetchCountries(value)
-        .then((countries) => {
-            if (countries.length > 10) {
-                Notify.info("Too many matches found. Please enter a more specific name.");
-                countryList.innerHTML = '';
-                countryInfoBox.innerHTML = '';
-            } else if (countries.length <= 10 && countries.length >= 2) {
-                const counreisItem = createItemsMarkup(countries);
-                countryList.innerHTML = counreisItem;
-                countryInfoBox.innerHTML = '';
-            } else if (countries.length === 1) {
-                countries.map((country) => {
-                    const createdInfo = createOneCountryInfo(country);
-                    countryInfoBox.innerHTML = createdInfo
-                    countryList.innerHTML = '';
-                })
-            }
-        })
-        .catch((error) => {
-            Notify.failure("Oops, there is no country with that name")
-            countryList.innerHTML = '';
-            countryInfoBox.innerHTML = '';
-        });
+    });
 }
 
-function createItemsMarkup(countries) {
-    return countries.map((country) => `<li class="item"><img src="${country.flags.svg}" alt="Flag" class="item-img" /><p>${country.name.official}</p></li>`).join("");
+function onAddItemsClick(evt) {
+    evt.preventDefault();
 }
 
-function createOneCountryInfo(country) {
-    return `<ul class="one-country-info"><li class="one-country-main-item"><img src="${country.flags.svg}" alt="Flag" class="one-country-item-img" /><p>${country.name.official}</p></li><li class="one-country-item"><p><span class="one-country-key">Capital:</span> ${country.capital}</p></li><li class="one-country-item"><p><span class="one-country-key">Population:</span> ${country.population}</p></li><li class="one-country-item"><p><span class="one-country-key">Languages:</span> ${Object.values(country.languages)}</p></li></ul>
-`
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
+$("#myBtn").click(function() { 
+ $('html, body').animate({
+ scrollTop: $("#header").offset().top 
+ }, 500); 
+});
