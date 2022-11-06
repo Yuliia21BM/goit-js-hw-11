e1bfd5003442c878685113419b1845258a0fec27
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-11-06 21:00:23 +0200

    add acsios

--- src/index.js
+++ src/index.js
@@ -19,8 +19,54 @@ imagesContainer.addEventListener('click', onAddItemsClick);
 searchForm.addEventListener('submit', onSearchFormSubmit);
 loadMoreBtn.addEventListener('click', onLoadMore);
 
+class PixabayAPI{
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        // this.limit = 500;
+    }
+
+    
+    async featchImages() {
+        const API_KEY = '31109678-013e606e285b36a60c72d34b0'
+        const url = `https://pixabay.com/api?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}&webformatURL`;
+        try {
+            const response = await axios.get(url);
+            return response.json();
+        } catch {
+        }
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1
+    }
+
+    // availableimages() {
+    //     if (this.page * 40 > this.limit) {
+    //         window.removeEventListener('scroll', onScrollWindow, {
+    //             passive: true
+    //         });
+    //         loadMoreBtn.classList.add('is-hidden');
+    //     }
+    // }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
+
 const pixabayAPI = new PixabayAPI();
 const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 200, });
+const axios = require('axios');
 
 async function onSearchFormSubmit(e) {
     e.preventDefault();
--- src/pixabay-servise.js
+++ src/pixabay-servise.js
@@ -1,42 +1,43 @@
-export default class PixabayAPI{
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        // this.limit = 500;
-    }
+// class PixabayAPI{
+//     constructor() {
+//         this.searchQuery = '';
+//         this.page = 1;
+//         // this.limit = 500;
+//     }
 
-    async featchImages() {
-        const API_KEY = '31109678-013e606e285b36a60c72d34b0'
-        const url = `https://pixabay.com/api?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}&webformatURL`;
-        try {
-            const response = await fetch(url);
-            return response.json();
-        } catch {
-        }
-    }
+    
+//     async featchImages() {
+//         const API_KEY = '31109678-013e606e285b36a60c72d34b0'
+//         const url = `https://pixabay.com/api?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}&webformatURL`;
+//         try {
+//             const response = await fetch(url);
+//             return response.json();
+//         } catch {
+//         }
+//     }
 
-    incrementPage() {
-        this.page += 1;
-    }
+//     incrementPage() {
+//         this.page += 1;
+//     }
 
-    resetPage() {
-        this.page = 1
-    }
+//     resetPage() {
+//         this.page = 1
+//     }
 
-    // availableimages() {
-    //     if (this.page * 40 > this.limit) {
-    //         window.removeEventListener('scroll', onScrollWindow, {
-    //             passive: true
-    //         });
-    //         loadMoreBtn.classList.add('is-hidden');
-    //     }
-    // }
+//     // availableimages() {
+//     //     if (this.page * 40 > this.limit) {
+//     //         window.removeEventListener('scroll', onScrollWindow, {
+//     //             passive: true
+//     //         });
+//     //         loadMoreBtn.classList.add('is-hidden');
+//     //     }
+//     // }
 
-    get query() {
-        return this.searchQuery;
-    }
+//     get query() {
+//         return this.searchQuery;
+//     }
 
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
-}
+//     set query(newQuery) {
+//         this.searchQuery = newQuery;
+//     }
+// }
